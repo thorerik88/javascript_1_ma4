0f06dab1ddf5e249f5432013b4744907618793d7
Thor-Erik <thorerik88@hotmail.com>
2019-12-12 19:47:17 +0100

    js cleanup

--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,31 +1,19 @@
 // CREATE A LIST OF TODO CARDS ON A WEBPAGE
 
-<<<<<<< HEAD
-// global variables
-function loopResults(json) {
-  const resultsElement = document.querySelector(".results");
-  console.log(json.length)
-=======
 let regexGreen = /false/;
 function loopResults(json) {
   const resultsElement = document.querySelector(".results");
->>>>>>> css
   for (let key in json) {
     // set output text for the div element inner html
     text = "<h1>" + "UserId: " + json[key].userId + "</h1>";
     text += "<h2>" + "id: " + json[key].id + "</h2>";
     text += "<p>" + "title: " + json[key].title + "</p>";
-<<<<<<< HEAD
-    text += "<p>" + "completed: " + json[key].completed + "</p>";
-
-=======
     // set red color to "false" and green value to "true"
     if (regexGreen.exec(json[key].completed)) {
       text += "<p>completed: <span class='red'>" + json[key].completed + "</span></p>";
     } else {
       text += "<p>completed: <span class='green'>" + json[key].completed + "</span></p>";
     }
->>>>>>> css
     // create a div element, add text and append to main div element
     let todo = document.createElement('div');
     todo.innerHTML = text;
