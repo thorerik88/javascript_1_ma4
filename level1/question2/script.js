0b5f5b1595e46cdb53171a6a2c60299a8da989bd
Thor-Erik <thorerik88@hotmail.com>
2019-12-15 12:40:38 +0100

    some markup changes after using JSLint

--- level1/question2/script.js
+++ level1/question2/script.js
@@ -5,9 +5,9 @@ function loopResults(json) {
   const resultsElement = document.querySelector(".results");
   for (let key in json) {
     // set output text for the div element inner html
-    text = "<h1>" + "UserId: " + json[key].userId + "</h1>";
-    text += "<h2>" + "id: " + json[key].id + "</h2>";
-    text += "<p>" + "title: " + json[key].title + "</p>";
+    text = "<h1>UserId: " + json[key].userId + "</h1>";
+    text += "<h2>id: " + json[key].id + "</h2>";
+    text += "<p>title: " + json[key].title + "</p>";
     // set red color to "false" and green value to "true"
     if (regexGreen.exec(json[key].completed)) {
       text += "<p>completed: <span class='red'>" + json[key].completed + "</span></p>";
@@ -15,7 +15,7 @@ function loopResults(json) {
       text += "<p>completed: <span class='green'>" + json[key].completed + "</span></p>";
     }
     // create a div element, add text and append to main div element
-    let todo = document.createElement('div');
+    let todo = document.createElement("div");
     todo.innerHTML = text;
     resultsElement.appendChild(todo);
   }
